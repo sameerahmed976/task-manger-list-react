92b603be8e1f3f6e61f877fdfb60eb490833a0d9
Mohammed Sameer Ahmed <sameerahmed976@gmail.com>
2022-09-22 21:37:35 +0530

    added the list

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,61 @@ import { useState } from "react";
 import "./style/style.css";
 
 function App() {
-  const [list, setList] = useState(["hello"]);
+  const [list, setList] = useState([
+    {
+      id: 1,
+      title: "hello",
+    },
+  ]);
+
+  const showAlert = (show = false, msg = "", value = "") => {
+    setAlert({
+      show,
+      msg,
+      value,
+    });
+  };
+
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: "",
+    value: "",
+  });
+
   const [name, setName] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (!name) {
+      alert("Please enter valid value");
+    } else {
+      const newList = {
+        id: new Date().getTime().toString(),
+        title: name,
+      };
+    }
+
+    // console.log(newList);
+    // console.log([...list, newList]);
+
+    setList([...list, newList]);
+    setName("");
+  };
+
+  const clearAll = () => {
+    setList([]);
   };
 
   return (
     <main className="App">
       <section className="task-manager__container">
+        {alert.show && (
+          <section className={`alert alert-${alert.value}`}>
+            {alert.msg}
+          </section>
+        )}
+
         <section className="task-manager__title">Task List Manager</section>
 
         <form onSubmit={handleSubmit} className="form">
@@ -32,11 +77,9 @@ function App() {
           {list.length > 0 ? (
             <section>
               {list.map((item) => {
-                const id = new Date().getTime().toString();
-
                 return (
-                  <article key={id} className="list__card">
-                    <h2>{item}</h2>
+                  <article key={item.id} className="list__card">
+                    <h2>{item.title}</h2>
                     <article className="btn btn--group">
                       <button className="btn btn--edit">edit</button>
                       <button className="btn btn--delete">delete</button>
@@ -44,6 +87,11 @@ function App() {
                   </article>
                 );
               })}
+              <section className="container">
+                <button className="btn btn-clear" onClick={clearAll}>
+                  clear button
+                </button>
+              </section>
             </section>
           ) : (
             <h1 className="no-list">Sorry no task list. Add new task list</h1>
